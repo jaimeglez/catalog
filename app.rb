ce84753e0ad7d61dda00c74bf28dd6af16914502
Jaime Gonzalez <jaime.gonzalez@tangosource.com>
2013-07-05 13:01:42 -0500

    added relation and added angular, and bootstrap to the project

--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
-require 'sinatra';
+require 'sinatra'
 require "sinatra/activerecord"
+require "./helpers"
 
-get '/' do
-  'this is the home page'
+get "/" do
+  @customers = Customer.order("created_at DESC")
+  erb :"customers/index"
 end
 
 
 set :database, "sqlite3:///catalog.db"
 
 class Customer < ActiveRecord::Base
+  has_many :services
 end
 
 class Service < ActiveRecord::Base
+  belongs_to :customer
 end
+
+
+
+# Respont_to JSON  
+# sin vistas por que es un cliente ,
---
+++ helpers.rb
@@ -0,0 +1,6 @@
+helpers do
+  def pretty_date(time)
+   time.strftime("%d %b %Y")
+  end
+end 
+
